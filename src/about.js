a98af2e28ceabf703723de952f6128c5ddeb7923
Shohei <shohei51@gmail.com>
2018-02-19 14:35:20 +0000

    Changed to sans-serif font, tweaked font size in about page

--- src/about.js
+++ src/about.js
@@ -28,11 +28,10 @@ class About extends Component {
                 <p>   
                     <img className='about-photo' src={profilePhoto} alt='me' />
                     My name is Shohei Shibata. Living in Düsseldorf with my wife and two boys,
-                    I work as a mechanical engineer in the automotive industry during the day,
-                    and code in the spare time.
+                    I code as much as I can outside of my day job responsibility as a mechanical engineer in the automotive industry.
                 </p>
                 <p>
-                    My biggest focus is in creating simple web apps that positively influence people's lives. I want the web to be useful, not distracting or addictive.
+                    My biggest focus is in creating simple web apps that positively influence people's lives. I want to help the web to be useful, rather than distracting or addictive.
                 </p>
                 <p>
                     If there are any projects which I could support you in any way, please don't hesitate to contact me.
