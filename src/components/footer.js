76259cd5d04f06c89089529ebc8bdd17979c5dec
Shohei <shohei.shibata.dev@gmail.com>
2024-01-12 07:00:31 -0500

    add footer nav for mobile, hide main nav for mobile

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,17 +1,42 @@
 import * as React from 'react'
 import { Link } from 'gatsby'
 import { globalHistory } from "@reach/router"
-import { footer, socialIcons, footerLinks, credits } from './footer.module.css'
+import { footer, socialIcons, footerLinks, credits, activeLink } from './footer.module.css'
+
+const navLinks = [
+  {
+    name: "Home",
+    to: "/",
+  },
+  {
+    name: "Work With Me",
+    to: "/work-with-me",
+  },
+  {
+    name: "Contact",
+    to: "/contact",
+  }
+]
 
 const Footer = () => {
   const path = globalHistory.location.pathname
   return (
     <footer className={footer}>        
-      <div className={footerLinks}>
-        { path !== "/" ? (
-          <Link to="/" className={footerLinks}>Back to Home</Link>
-        ) : null}
-      </div>
+      <nav className={footerLinks}>
+        <ul>
+            {navLinks.map(link => (
+              <li key={link.to}>
+                <Link
+                  to={link.to}
+                  className={footerLinks}
+                  activeClassName={activeLink}
+                >
+                  {link.name}
+                </Link>
+              </li>
+            ))}
+          </ul>
+      </nav>
       <div className={credits}>
         <div>© Shohei Shibata 2023</div>
         <div className={socialIcons}>
--- src/components/layout.js
+++ src/components/layout.js
@@ -3,20 +3,43 @@ import { Link } from 'gatsby'
 import Footer from './footer'
 import Box from './box'
 import { globalHistory } from "@reach/router"
-import { container, nav, logo, main } from './layout.module.css'
+import { container, navDesktop, activeLink, logo, main } from './layout.module.css'
 import "../styles/global.css"
+import "../styles/variables.css"
 import './btn.css'
+
+const navLinks = [
+  {
+    name: "Home",
+    to: "/",
+  },
+  {
+    name: "Work With Me",
+    to: "/work-with-me",
+  },
+  {
+    name: "Contact",
+    to: "/contact",
+  }
+]
 
 const Layout = ({ children }) => {
   const path = globalHistory.location.pathname
   return (
     <div className={container}>
-      <nav className={nav}>
+      <nav className={navDesktop}>
         <h1 className={logo}><Link to="/">Shohei Shibata</Link></h1>
         <ul>
-          <li><Link to="/">Home</Link></li>
-          <li><Link to="/work-with-me">Work With Me</Link></li>
-          <li><Link to="/contact">Contact</Link></li>
+          {navLinks.map(link => (
+            <li key={link.to}>
+              <Link
+                to={link.to}
+                activeClassName={activeLink}
+              >
+                {link.name}
+              </Link>
+            </li>
+          ))}
         </ul>
       </nav>
       <main className={main}>
